c1783be7191dbe3c523e900f5eeea14e78bbf871
Vinicius Mendite <vinicius.miff@gmail.com>
2020-03-31 23:56:55 -0300

    Layout feito

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Content from './components/Content';
+
+function App() {
+  return (
+    
+      <Content />
+  
+  );
+}
+
+export default App;
